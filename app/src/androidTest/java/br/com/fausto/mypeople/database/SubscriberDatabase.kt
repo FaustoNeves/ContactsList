5eda143e2ae6193d3a132e69730b2081bda16672
Fausto Neves Kina <faustonkina@gmail.com>
2021-04-19 18:14:36 -0300

    More database tests. Next step is how to automate these tests

--- app/src/androidTest/java/br/com/fausto/mypeople/database/SubscriberDatabase.kt
+++ app/src/androidTest/java/br/com/fausto/mypeople/database/SubscriberDatabase.kt
@@ -9,7 +9,7 @@ import br.com.fausto.mypeople.repository.subscriber.RSubscriber
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.equalTo
-import org.junit.Assert.assertThat
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,22 +26,54 @@ class SubscriberDatabase {
         subscriberDB =
             SubscriberDatabase.getInstance(InstrumentationRegistry.getInstrumentation().targetContext).subscriberDAO
         repository = RSubscriber(subscriberDB)
+        runBlocking {
+            repository.deleteAll()
+        }
     }
 
-//    @After
-//    fun endDB() {
-//        subscriberDB.close()
-//    }
-
     @Test
-    fun insertData() = runBlocking {
-        repository.deleteAll()
-        val subscriber = Subscriber(5, "name", "email", "123")
+    fun insertAndSearchById() = runBlocking {
+        val subscriber = Subscriber(5, "Ann", "ann@hotmail.com", "123")
         repository.insert(subscriber)
         val newSubscriber = repository.searchById(5)
         assertThat(subscriber.name, equalTo(newSubscriber.name))
-
     }
+
+    @Test
+    fun insertAndUpdate() = runBlocking {
+        val subscriber = Subscriber(3, "Abel", "abel@gmail.com", "456")
+        repository.insert(subscriber)
+        subscriber.phoneNumber = "789"
+        repository.update(subscriber)
+        val newSubscriber = repository.searchById(3)
+        assertThat(subscriber.phoneNumber, equalTo(newSubscriber.phoneNumber))
+    }
+
+    @Test
+    fun inserAndDelete() = runBlocking {
+        val subscriber = Subscriber(6, "Halo", "halo@outlook.com", "246")
+        repository.insert(subscriber)
+        val allSubscribers = repository.subscribers
+        assertNull(allSubscribers.value)
+    }
+
+    @Test
+    fun getAllSubscribers() = runBlocking {
+        val subscriber = Subscriber(10, "Juan", "juan@hotmail.com", "123")
+        val subscriber2 = Subscriber(15, "Camila", "camila@hotmail.com", "456")
+        val subscriber3 = Subscriber(20, "Mona", "mona@hotmail.com", "172")
+        val subscriber4 = Subscriber(25, "Rafael", "rafael@hotmail.com", "987")
+        repository.insert(subscriber)
+        repository.insert(subscriber2)
+        repository.insert(subscriber3)
+        repository.insert(subscriber4)
+        val allSubscribers = repository.subscribers
+        assertNotNull(allSubscribers)
+    }
+//    @Test
+//    fun insertAndDelete() = runBlocking {
+//
+//    }
 
 //    @Test
 //    fun getBufferoosRetrievesData() {
